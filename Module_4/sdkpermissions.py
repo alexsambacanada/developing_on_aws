1473ffeb16e55f2b0b40e9ec5a26a147284890b3
EC2 Default User <ec2-user@ip-172-31-7-249.ec2.internal>
2023-11-04 13:26:29 +0000

    added commented section

--- Module_4/sdkpermissions.py
+++ Module_4/sdkpermissions.py
@@ -12,26 +12,27 @@ role_arn = 'arn:aws:iam::475815351148:role/developing-on-aws-mod-kinesis-ro'
 
 try:
     
-    # Assuming the IAM role
-    response = sts_client.assume_role(
-        RoleArn=role_arn,
-        RoleSessionName='AssumeKinesisRoleSession'
-    )
     
-    print(f"Temp Access Key ID: {response['Credentials']['AccessKeyId']}")
-    print(f"Temp Secret Access Key ID: {response['Credentials']['SecretAccessKey']}")
-    print(f"Expiration: {response['Credentials']['Expiration']}")
+    # # Assuming the IAM role
+    # response = sts_client.assume_role(
+    #     RoleArn=role_arn,
+    #     RoleSessionName='AssumeKinesisRoleSession'
+    # )
+    
+    # print(f"Temp Access Key ID: {response['Credentials']['AccessKeyId']}")
+    # print(f"Temp Secret Access Key ID: {response['Credentials']['SecretAccessKey']}")
+    # print(f"Expiration: {response['Credentials']['Expiration']}")
 
-    # Extract temporary credentials
-    credentials = response['Credentials']
+    # # Extract temporary credentials
+    # credentials = response['Credentials']
 
-    # Create a new session using the temporary credentials
-    session = boto3.Session(
-        aws_access_key_id=credentials['AccessKeyId'],
-        aws_secret_access_key=credentials['SecretAccessKey'],
-        aws_session_token=credentials['SessionToken'],
-        region_name='us-east-1'
-    )
+    # # Create a new session using the temporary credentials
+    # session = boto3.Session(
+    #     aws_access_key_id=credentials['AccessKeyId'],
+    #     aws_secret_access_key=credentials['SecretAccessKey'],
+    #     aws_session_token=credentials['SessionToken'],
+    #     region_name='us-east-1'
+    # )
 
     # Create a Kinesis client using the new session
     kinesis_client = session.client('kinesis')
